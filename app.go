133c18b857138cb1302b07aa86a5de07ff97381b
Brian McClain <brianmmcclain@gmail.com>
2019-01-02 10:54:01 -0500

    Switch demo to use kubernetes event source to be more straight forward

--- app.go
+++ app.go
@@ -1,30 +1,18 @@
 package main
 
 import (
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-type Message struct {
-	ID          string `json:"ID"`
-	Data        string `json:"Data"`
-	PublishTime string `json:"PublishTime"`
-}
-
 func handlePost(rw http.ResponseWriter, req *http.Request) {
 	defer req.Body.Close()
 	body, _ := ioutil.ReadAll(req.Body)
 
-	var message Message
-	json.Unmarshal(body, &message)
-	sData, _ := base64.StdEncoding.DecodeString(message.Data)
-
-	fmt.Fprintf(rw, "[%s %s] %s", message.PublishTime, message.ID, sData)
-	log.Printf("[%s %s] %s", message.PublishTime, message.ID, sData)
+	fmt.Fprintf(rw, "%s", body)
+	log.Printf("%s", body)
 }
 
 func main() {
